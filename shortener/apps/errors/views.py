c010ce3dfab380a47c6309688fc4d8a31195b63a
Anup Bagali <anup.bagali1@gmail.com>
2020-07-09 17:37:05 -0400

    Update views.py

--- shortener/apps/errors/views.py
+++ shortener/apps/errors/views.py
@@ -2,10 +2,5 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def handle_404_view(request: HttpRequest, exception) -> HttpResponse:
-    del exception
-    return render(request, "404.html", status=404)
-
-
 def handle_500_view(request: HttpRequest) -> HttpResponse:
     return render(request, "error/500.html", status=500)
