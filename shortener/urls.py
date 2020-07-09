bf69bf4520c63e2d83378042b4a7649db759d43f
Anup Bagali <anup.bagali1@gmail.com>
2020-07-09 17:38:06 -0400

    Update urls.py

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .apps.errors.views import handle_404_view, handle_500_view
+from .apps.errors.views import handle_500_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
